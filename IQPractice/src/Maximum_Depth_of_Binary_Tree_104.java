32b6aa06cfedc3faf33dae1507c848cbdbd1158f
HouyiFan <fanosu.685@gmail.com>
2018-04-16 00:20:12 -0400

    Add files via upload

---
+++ IQPractice/src/Maximum_Depth_of_Binary_Tree_104.java
@@ -0,0 +1,36 @@
+
+public class Maximum_Depth_of_Binary_Tree_104 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static void main(String[] args) {
+        //       3
+        //    9     20
+        //        15   7
+        TreeNode tree = new TreeNode(3);
+        tree.left = new TreeNode(9);
+        tree.right = new TreeNode(20);
+        tree.right.left = new TreeNode(15);
+        tree.right.right = new TreeNode(7);
+        System.out.println(maxDepth(tree));
+    }
+
+}
