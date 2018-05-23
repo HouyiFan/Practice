2108a594af91cd3378d84ad435b8e31559ac6107
Houyi Fan <fanosu.685@gmail.com>
2018-05-23 18:38:31 -0400

    Update Best_Time_to_Buy_and_Sell_Stock_121.java

--- IQPractice/src/Best_Time_to_Buy_and_Sell_Stock_121.java
+++ IQPractice/src/Best_Time_to_Buy_and_Sell_Stock_121.java
@@ -1,9 +1,9 @@
 
 public class Best_Time_to_Buy_and_Sell_Stock_121 {
-
-    public int maxProfit(int[] prices) {
-        //Time: O(n)
-        //Space: O(1)
+    
+    //Time: O(n)
+    //Space: O(1)
+    public int maxProfit(int[] prices) {   
         int max = 0;
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
@@ -18,9 +18,9 @@ public class Best_Time_to_Buy_and_Sell_Stock_121 {
     }
 
     //Kadane's Algorithm
+    //Time: O(n)
+    //Space: O(1)
     public int maxProfit2(int[] prices) {
-        //Time: O(n)
-        //Space: O(1)
         int maxCur = 0, maxSoFar = 0;
         for (int i = 1; i < prices.length; i++) {
             maxCur = Math.max(0, maxCur += prices[i] - prices[i - 1]);
@@ -30,7 +30,6 @@ public class Best_Time_to_Buy_and_Sell_Stock_121 {
     }
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         System.out.println(new Best_Time_to_Buy_and_Sell_Stock_121()
                 .maxProfit(new int[] { 1, -1, 2 }));
         System.out.println(new Best_Time_to_Buy_and_Sell_Stock_121()
